35c6588e76fe2a8932973cfdecd9ba83ba238a09
Ignacio Huerta <ignacio@ihuerta.net>
2014-12-03 16:15:41 +0100

    Sandbox button: add a nice big button that executes the Ajax call

--- copsub/lib/sandbox_button.php
+++ copsub/lib/sandbox_button.php
@@ -1,11 +1,31 @@
 <?php
 
+  // Menu Button
   add_action('admin_menu', 'csb_menu');
 
   function csb_menu() {
     add_submenu_page( 'tools.php', "Copsub Sandbox button", "Copsub Sandbox button", 'manage_options', 'copsub_sandbox_button', 'csb_render_admin_page');
   }
 
+
+
+
+
+  // Backend Actions
+  add_action( 'wp_ajax_csb_action', 'csb_action_callback' );
+
+  function csb_action_callback() {
+    global $wpdb;
+
+    echo "Sandbox Mode actions executed successfully";
+
+    die(); // this is required to terminate immediately and return a proper response
+  }
+
+
+
+
+  // Admin page with Ajax button
   function csb_render_admin_page() { ?>
     <h1>Copsub Sandbox button</h1>
 
@@ -18,37 +38,24 @@
       <li>Randomises email addresses, to avoid errors like sending test notifications. Users with admin role are excluded.</li>
     </ol>
 
+    <a class="button button-primary" style="font-size:32px; padding:20px; margin: 40px; height: 70px"
+       onclick="executeSandboxActions()">Sandbox Button</a>
+
     <script type="text/javascript" >
-      jQuery(document).ready(function($) {
+      var data = {
+        'action': 'csb_action'
+      };
 
-        var data = {
-          'action': 'my_action',
-          'whatever': 1234
-        };
-
-        // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-        $.post(ajaxurl, data, function(response) {
-          alert('Got this from the server: ' + response);
+      // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+      function executeSandboxActions(){
+        jQuery.post(ajaxurl, data, function(response) {
+          alert(response);
         });
-      });
+        return false;
+      };
     </script>
 
     <?php
-  }
-
-
-  add_action( 'wp_ajax_my_action', 'my_action_callback' );
-
-  function my_action_callback() {
-    global $wpdb; // this is how you get access to the database
-
-    $whatever = intval( $_POST['whatever'] );
-
-    $whatever += 10;
-
-          echo $whatever;
-
-    die(); // this is required to terminate immediately and return a proper response
   }
 
 ?>
