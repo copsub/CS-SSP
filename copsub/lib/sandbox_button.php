6f3dadca875c1c05c3879bf05d932ab73a51734d
Ignacio Huerta <ignacio@ihuerta.net>
2014-12-03 18:00:41 +0100

    Limit the sandbox button to sandbox environments

--- copsub/lib/sandbox_button.php
+++ copsub/lib/sandbox_button.php
@@ -15,12 +15,30 @@
   add_action( 'wp_ajax_csb_action', 'csb_action_callback' );
 
   function csb_action_callback() {
+    // Disable automatic updraft backups
+    update_option("updraft_interval", "manual" );
+    update_option("updraft_interval_database", "manual" );
+
+    // Set Seamless Donations Paypal environment to Sandbox
+    update_option("dgx_donate_paypal_server", "SANDBOX" );
+
+    // Enable password to access the website
+    update_option("password_protected_status", "1");
+
+    // Randomise the email address and usernames (except for admin users)
     global $wpdb;
+    $wpdb->get_results("
+      UPDATE ".$wpdb->prefix."users
+      LEFT JOIN ".$wpdb->prefix."usermeta m1 ON ".$wpdb->prefix."users.ID = m1.user_id
+      SET user_email = CONCAT(FLOOR(rand() * 10000000),'@mailinator.com'), user_login = FLOOR(rand() * 10000000)
+      WHERE m1.meta_key = '".$wpdb->prefix."capabilities' AND m1.meta_value NOT LIKE '%administrator%'
+    ");
 
     echo "Sandbox Mode actions executed successfully";
-
     die(); // this is required to terminate immediately and return a proper response
   }
+
+
 
 
 
@@ -37,6 +55,10 @@
       <li>Locks the website so a password is required to access it</li>
       <li>Randomises email addresses, to avoid errors like sending test notifications. Users with admin role are excluded.</li>
     </ol>
+
+    <?php if (strpos($_SERVER[HTTP_HOST],'sandbox') !== false || isset($_GET["force_sandbox_button"])) { ?>
+
+    <p style="padding:10px; background-color: #FF6767;"><strong>This button will modify permanently the emails of the non-admin users, turning them into something like "9217758@mailinator.com". Please check that you have a backup of the database before going ahead.</strong></p>
 
     <a class="button button-primary" style="font-size:32px; padding:20px; margin: 40px; height: 70px"
        onclick="executeSandboxActions()">Sandbox Button</a>
@@ -55,7 +77,10 @@
       };
     </script>
 
+    <?php } else { ?>
+      <p style="padding:10px; background-color: #FF6767;"><strong>To avoid losing production data, this button will only work in wordpress installation under a domain containing the word"sandbox".<br/><br/> If you are completely sure of what you are doing you can add the parameter "force_sandbox_button=true" to the current URL.</strong></p>
     <?php
+    }
   }
 
 ?>
